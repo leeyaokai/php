3b435ff7a70143b0cb73f46bda259a600d8a1ab3
leeyaokai <30333131+leeyaokai@users.noreply.github.com>
2017-08-04 08:40:35 +0800

    使用者登入畫面

--- 08-09EX/08-09-1.php
+++ 08-09EX/08-09-1.php
@@ -7,10 +7,12 @@
     <title>Form1</title>
 </head>
 <body>
+    
     <form action="08-09-2.php" method="post">
         <input type="text" name="Name" size="10" placeholder="Name" /><br/>
         <input type="password" name="password" size="10" placeholder="Password" /><br/>
         <input type="submit" name="submit" />
     </form>
+    
 </body>
 </html>
